c2e8111cb3bc76b8d86fba6f83e64f8ee880b8f8
moofat7y <mohammedbelal802@gmail.com>
2023-08-26 19:58:45 +0300

    thrid commit

--- src/app/(root)/shop/page.jsx
+++ src/app/(root)/shop/page.jsx
@@ -2,9 +2,5 @@ import server from "@/utils/server";
 import React from "react";
 
 export default async function page() {
-  const res = await fetch(`${server}/api/auth/signup`, {
-    method: "GET",
-    cache: "no-cache",
-  });
   return <div>shop</div>;
 }
